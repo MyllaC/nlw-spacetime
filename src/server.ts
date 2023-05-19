e76ad6b6050aec6973fa5e3c114b9e524729cef9
Camila Cristina <camila.alves@nectarcrm.com.br>
2023-05-19 14:02:22 -0300

    Aula 03 - Integrando UI com bibliotecas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,22 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas as urls de fron podem acessar nosso backend mas o ideal e colcoar exatamente os enderecos com a url de producao e de desenvolvimento
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
