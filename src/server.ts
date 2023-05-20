a0913f61361ca5019755f000f646ef5fe5bfbf4f
Camila Cristina <camila.alves@nectarcrm.com.br>
2023-05-20 17:07:38 -0300

    Integrando os projetos web e mobile

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,34 @@
 import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(cors, {
-  origin: true, // todas as urls de fron podem acessar nosso backend mas o ideal e colcoar exatamente os enderecos com a url de producao e de desenvolvimento
+  origin: true,
 })
 
 app.register(jwt, {
   secret: 'spacetime',
 })
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 
 app
   .listen({
